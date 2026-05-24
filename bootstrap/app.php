8c27f8c7aeae54b67ca675d9dfd8f96df8731f93
PMMIS <PMMISTrader@gmail.com>
2026-05-24 16:23:03 +0330

    OptimizeResponse modular middleware + Category module with Persian slug support

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,9 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\OptimizeResponse\OptimizeHtml;
+use App\Http\Middleware\OptimizeResponse\AddSecurityHeaders;
+use App\Http\Middleware\OptimizeResponse\AddCacheHeaders;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,11 +14,17 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->append(\App\Http\Middleware\OptimizeResponse::class);
-        $middleware->append(\App\Http\Middleware\DecodeSlug::class);
+        // Add security and optimization middleware in correct order
+        $middleware->append(OptimizeHtml::class);
+        $middleware->append(AddSecurityHeaders::class);
+        
+        // CacheGuestResponses is already added via web group
         $middleware->web(append: [
-          \App\Http\Middleware\CacheGuestResponses::class,
+            AddCacheHeaders::class,
+            \App\Http\Middleware\CacheGuestResponses::class,
         ]);
+        
+        $middleware->append(\App\Http\Middleware\DecodeSlug::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
